154cc25307eb73641849fb8b554a3251b49680ea
DayaMathew <dayamathew29@gmail.com>
2025-12-30 08:21:52 +0530

    Add files via upload

---
+++ interchange1.java
@@ -0,0 +1,11 @@
+public class interchange1{
+	public static void main(String[]args)
+	{
+		int a=10,b=20;
+		System.out.println("before swapping \n a= "+ a +  " b = " +b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println(" after swapping \n a= "+ a + " b = " + b);
+	}
+}
